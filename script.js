94bc46bbf6c4112301eeeeae17f033979951c76f
Krishna Dhote <120446388+dhotekrishna@users.noreply.github.com>
2024-01-15 23:16:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,19 +3,20 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+
 function page4Animation() {
     var elemC = document.querySelector("#elem-container")
     var fixed = document.querySelector("#fixed-image")
-    elemC.addEventListener("mouseenter",function(){
+    elemC.addEventListener("mouseenter", function () {
         fixed.style.display = "block"
     })
-    elemC.addEventListener("mouseleave",function(){
+    elemC.addEventListener("mouseleave", function () {
         fixed.style.display = "none"
-    }) 
+    })
 
     var elems = document.querySelectorAll(".elem")
-    elems.forEach(function(e){
-        e.addEventListener("mouseenter",function(){
+    elems.forEach(function (e) {
+        e.addEventListener("mouseenter", function () {
             var image = e.getAttribute("data-image")
             fixed.style.backgroundImage = `url(${image})`
         })
@@ -29,5 +30,33 @@ function swiperAnimation() {
         spaceBetween: 100,
     });
 }
+function menuAnimation() {
+
+    var menu = document.querySelector("nav h3")
+    var full = document.querySelector("#full-scr")
+    var navimg = document.querySelector("nav img")
+    var flag = 0
+    menu.addEventListener("click", function () {
+        if (flag == 0) {
+            full.style.top = 0
+            navimg.style.opacity = 0
+            flag = 1
+        } else {
+            full.style.top = "-100%"
+            navimg.style.opacity = 1
+            flag = 0
+        }
+    })
+}
+
+function loaderAnimation() {
+    var loader = document.querySelector("#loader")
+    setTimeout(function () {
+        loader.style.top = "-100%"
+    }, 4200)
+}
+
 swiperAnimation()
 page4Animation()
+menuAnimation()
+loaderAnimation()
